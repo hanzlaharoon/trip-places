294ea762595225de40aa86c8e638f5c1f4857826
Hanzla Haroon <hanzla.haroon@outlook.com>
2021-06-21 18:06:02 +0500

    Added main, navbar, card components

---
+++ src/components/Main.js
@@ -0,0 +1,67 @@
+import { Box, Grid } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+import PlaceCard from './PlaceCard';
+
+const Main = () => {
+  const [serachQuery, setSerachQuery] = useState('');
+  const [placesList, setPlacesList] = useState();
+
+  // Populate Data
+  useEffect(() => {
+    const data = [
+      {
+        id: 1,
+        name: 'F9 Park',
+        location: 'F9, Islamabad',
+        description: 'Family Park',
+        isFavorite: true,
+      },
+      {
+        id: 2,
+        name: 'Centorus Mall',
+        location: 'F8, Islamabad',
+        description: 'Shopping Mall',
+        isFavorite: false,
+      },
+      {
+        id: 3,
+        name: 'KFC, F10 Brach',
+        location: 'F10 Markaz, Islamabad',
+        description: 'Fast Food',
+        isFavorite: false,
+      },
+    ];
+
+    setPlacesList(data);
+  }, []);
+
+  function toggleFavorite(value) {
+    //   Api Call
+  }
+
+  return (
+    <>
+      <Navbar
+        serachQuery={serachQuery}
+        handleSerachQuery={(e) => setSerachQuery(e.target.value)}
+      />
+
+      <Box padding={1} margin={1}></Box>
+
+      <Box padding={1} margin={1}>
+        <Grid container spacing={4}>
+          {/* {!placesList && 'Add Places to see here!'} */}
+          {placesList &&
+            placesList.map((place) => (
+              <Grid key={place.id} item xs={12} sm={6} md={3}>
+                <PlaceCard place={place} toggleFavorite={toggleFavorite} />
+              </Grid>
+            ))}
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/components/PlaceCard.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { IconButton, makeStyles } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+const PlaceCard = ({
+  place: { name, location, description, isFavorite },
+  toggleFavorite,
+}) => {
+  const classes = useStyles();
+  return (
+    <>
+      <Card>
+        <CardContent>
+          <Typography variant='h5' component='h2'>
+            {name}
+          </Typography>
+          <Typography className={classes.pos} color='textSecondary'>
+            {location}
+          </Typography>
+          <Typography variant='body2' component='p'>
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size='small' onClick={() => toggleFavorite(isFavorite)}>
+            {isFavorite ? 'Remove Favorite' : 'Add to Favorite'}
+          </Button>
+          <IconButton aria-label='add to favorites'>
+            <FavoriteIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </>
+  );
+};
+
+export default PlaceCard;
