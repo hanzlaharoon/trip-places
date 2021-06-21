d440292d07d9c4b5eda3562bd9ea4c9c5f72f290
Hanzla Haroon <hanzla.haroon@outlook.com>
2021-06-22 01:29:53 +0500

    server: Added user router

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/usersRouter');
 
 const url = 'mongodb://localhost:27017/tripPlaces';
 const connect = mongoose.connect(url, {
---
+++ server/routes/userRouter.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+let userSchema = require('../models/users');
+
+router.post('/register', function (req, res, next) {
+  userSchema
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if (user != null) {
+        var err = new Error('User ' + req.body.email + ' already exists!');
+        err.status = 403;
+        next(err);
+      } else {
+        return userSchema.create({
+          email: req.body.email,
+          name: req.body.name,
+          password: req.body.password,
+        });
+      }
+    })
+    .then(
+      (user) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ status: 'Registration Successful!', user: user });
+      },
+      (err) => next(err)
+    )
+    .catch((err) => next(err));
+});
+
+router.get('/login', function (req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  userSchema
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if (user == null) {
+        var err = new Error('email and password does not match!');
+        err.status = 403;
+        return next(err);
+      } else if (user.password !== password) {
+        var err = new Error('Your password is incorrect!');
+        err.status = 403;
+        return next(err);
+      } else if (user.email === email && user.password === password) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ status: 'Login Successful!' });
+      } else {
+        var err = new Error('Unknown Error');
+        err.status = 403;
+        return next(err);
+      }
+    })
+    .catch((err) => next(err));
+});
+
+module.exports = router;
