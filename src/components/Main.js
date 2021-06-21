a36b9fb719a19cf3ddf9ce18ee97e645d9402431
Hanzla Haroon <hanzla.haroon@outlook.com>
2021-06-21 18:44:50 +0500

    Main: Added favorites filtering

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
-import { Box, Grid } from '@material-ui/core';
+import { Box, Button, Grid } from '@material-ui/core';
+import { Favorite } from '@material-ui/icons';
 import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import PlaceCard from './PlaceCard';
@@ -6,6 +7,7 @@ import PlaceCard from './PlaceCard';
 const Main = () => {
   const [serachQuery, setSerachQuery] = useState('');
   const [placesList, setPlacesList] = useState();
+  const [showFavorites, setShowFavorites] = useState(false);
 
   // Populate Data
   useEffect(() => {
@@ -36,7 +38,7 @@ const Main = () => {
     setPlacesList(data);
   }, []);
 
-  function toggleFavorite(value, id) {
+  function toggleFavoritePlace(value, id) {
     //   Api Call
   }
 
@@ -47,17 +49,39 @@ const Main = () => {
         handleSerachQuery={(e) => setSerachQuery(e.target.value)}
       />
 
-      <Box padding={1} margin={1}></Box>
+      <Box padding={1} margin={1}>
+        <Button
+          variant={!showFavorites ? 'outlined' : ''}
+          onClick={() => setShowFavorites(false)}
+        >
+          All
+        </Button>
+        <Button
+          variant={showFavorites ? 'outlined' : ''}
+          onClick={() => setShowFavorites(true)}
+        >
+          Favorites
+        </Button>
+      </Box>
 
       <Box padding={1} margin={1}>
         <Grid container spacing={4}>
           {/* {!placesList && 'Add Places to see here!'} */}
           {placesList &&
-            placesList.map((place) => (
-              <Grid key={place.id} item xs={12} sm={6} md={3}>
-                <PlaceCard place={place} toggleFavorite={toggleFavorite} />
-              </Grid>
-            ))}
+            placesList
+              .filter((place) => {
+                if (showFavorites && place.isFavorite) {
+                  return place;
+                } else if (!showFavorites) return place;
+              })
+              .map((place) => (
+                <Grid key={place.id} item xs={12} sm={6} md={3}>
+                  <PlaceCard
+                    place={place}
+                    toggleFavorite={toggleFavoritePlace}
+                  />
+                </Grid>
+              ))}
         </Grid>
       </Box>
     </>
